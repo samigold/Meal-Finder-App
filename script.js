63cd377a3c0b0c72c484e599c203cbe43b6f8f59
Samuel Madu <info@samuelmadu.com>
2024-10-15 21:07:28 +0100

    Implement meal search functionality
    - Added searchMeal function to fetch and display meals from the API based on the search term.
    - Cleared the single meal display before performing a new search.
    - Added event listener to trigger the search on form submission.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+ const search = document.getElementById('search'),
+submit = document.getElementById('submit'),
+random = document.getElementById('random'),
+mealsEl = document.getElementById('meals'),
+resultHeading = document.getElementById('result-heading')
+single_mealEL = document.getElementById('single-meal');
 
+//Search meal and fetch from api
+function searchMeal(e) {
+    e.preventDefault();
+
+    //Clear single meal
+    single_mealEL.innerHTML = '';
+
+    //Get search term
+    const term = search.value;
+
+     if(term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+        })
+     }
+}
+
+//Event listeners
+submit.addEventListener('submit', searchMeal);
