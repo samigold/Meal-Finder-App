62cd604324681993fc7abe8cccdf715117aab35f
Samuel Madu <info@samuelmadu.com>
2024-10-16 03:09:54 +0100

    Refactor .meals class to use Flexbox for layout
    - Changed .meals class from CSS Grid to Flexbox for better responsiveness.
    - Updated .meals class to use flex properties for layout. Fix meal search display issue
    
    - Corrected the mapping of meal data to display meals in a grid format.
    - Ensured that the search results are properly cleared before displaying new results.
    - Added event listener for the search form submission.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 submit = document.getElementById('submit'),
 random = document.getElementById('random'),
 mealsEl = document.getElementById('meals'),
-resultHeading = document.getElementById('result-heading')
+resultHeading = document.getElementById('result-heading'),
 single_mealEL = document.getElementById('single-meal');
 
 //Search meal and fetch from api
@@ -20,7 +20,31 @@ function searchMeal(e) {
         .then(res => res.json())
         .then(data =>{
             console.log(data);
-        })
+
+            resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
+
+            if(data.meals === null){
+                resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+
+                mealsEl.innerHTML = '';
+            } else{
+                mealsEl.innerHTML = data.meals
+                .map(
+                    meal =>`
+                    <div class="meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
+                    `
+                )
+                .join('');
+            }
+        });
+        search.value = '';
+     } else {
+        alert("Please enter a search term");
      }
 }
 
